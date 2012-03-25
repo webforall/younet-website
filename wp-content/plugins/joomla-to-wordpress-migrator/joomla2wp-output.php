535efc26ad1983fadc552b3c27907349f750e104
Francesco Tassi <ftassi@MacBook-Pro-di-Francesco-Tassi.local>
2012-03-25 16:37:39 +0200

    installato plugin migrazione joomla

---
+++ wp-content/plugins/joomla-to-wordpress-migrator/joomla2wp-output.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+function j2wp_print_output_page()
+{
+  echo '  <div class="j2wp_status_page">' . "\n";
+  echo '   <br />' . "\n";
+  echo '   <h3>Joomla To Wordpress Migration - Status Messages</h3>' . "\n";
+  echo '   <br /><br />' . "\n";
+  echo '  </div>' . "\n";
+
+  flush();
+
+  return;
+}
+
+
+?>
